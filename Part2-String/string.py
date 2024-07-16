aa626d394e6b8d13d90dcee1c8d5a96d8d340383
Aiproland <uplimitopenai@gmail.com>
2024-07-16 19:20:15 +0330

    Add files via upload

---
+++ Part2-String/string.py
@@ -0,0 +1,12 @@
+name = "rOghaaye "
+family = "farAji"
+full_name = name+family
+# print(full_name.title())
+# print(full_name.upper())
+# print(full_name.replace('f' , '*'))
+# print(len(name))
+# print(len(name.strip()))
+# print(full_name.split())
+# print(f'welcome {name}')
+# print(name.count('a'))
+print(full_name.find('far'))
